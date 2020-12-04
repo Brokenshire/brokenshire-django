c5cf155cc94bb554a879422cc2ba6a1bfebbd0a8
Jack Brokenshrie <Jackb1@xtra.co.nz>
2020-12-04 20:07:25 +1300

    updating docstrings

--- manage.py
+++ manage.py
@@ -12,7 +12,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Django's command-line utility for administrative tasks."""
+
+# Django's command-line utility for administrative tasks.
+
+# Local imports
 import os
 import sys
 
