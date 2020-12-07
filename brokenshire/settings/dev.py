b96a7367ba6fbe3d99f2ca70ecc19597e5f06bd8
Jack Brokenshrie <Jackb1@xtra.co.nz>
2020-12-07 21:58:19 +1300

    Update dev.py

--- brokenshire/settings/dev.py
+++ brokenshire/settings/dev.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Local import
+import os
+
 # Third-party imports
 from dotenv import load_dotenv
 
