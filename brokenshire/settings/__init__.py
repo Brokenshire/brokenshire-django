81a105330067002e4ceb9e782b4d67f60514200e
Jack Brokenshrie <Jackb1@xtra.co.nz>
2020-12-08 17:28:11 +1300

    settings change

--- brokenshire/settings/__init__.py
+++ brokenshire/settings/__init__.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 # Local imports
-import os
 from .base import *
 
 try:
--- brokenshire/settings/dev.py
+++ brokenshire/settings/dev.py
@@ -15,9 +15,6 @@
 # Local import
 import os
 
-# Third-party imports
-from dotenv import load_dotenv
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -45,8 +42,5 @@ DATABASES = {
 }
 
 
-# Load the .env file containing local environment variables
-load_dotenv()
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
